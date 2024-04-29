a6258448422280042d307d50f6e5bb3a9ce4a4f9
Tom Stirnkorb <tom@stirnkorb.me>
2024-04-29 13:53:19 +0000

    BFS working

--- exercise_3/pathfinder.m
+++ exercise_3/pathfinder.m
@@ -1,24 +1,55 @@
 obstacle_probability = 0.3;
-size = 20;
+board_size = 20;
 
-b = make_board(size, obstacle_probability);
+b = make_board(board_size, obstacle_probability);
 
+% find all non-obstacle nodes at the initial row
+start_nodes_x = find(b(1,:) ~= 1);
 
+% matrix that stores whether a node has been explored
+explored = zeros(board_size);
+
+% add all start nodes to frontier
+frontier = {};
+for i = 1:size(start_nodes_x, 2)
+    x = start_nodes_x(i);
+    coords = {[x, 1]};
+    frontier = [frontier, coords];
+end
+
+while numel(frontier) > 0
+    node = frontier{1};
+    explored(node(1), node(2)) = 1;
+    if node(2) == board_size
+        fprintf("Found goal node!")
+        disp(node)
+        break
+    end
+    frontier(1) = [];
+    nb = get_neighbours(b, node(1), node(2));
+    % add all unexplored neighbours to frontier
+    for i = 1:size(nb, 2)
+        if explored(nb{i}(1), nb{i}(2)) == 0
+            frontier = [frontier, nb(i)];
+        end
+    end
+end
 
 function neighbours = get_neighbours(board, x, y)
     % create all possible moves
-    neighbours{1} = [x+1, y];
-    neighbours{2} = [x, y+1];
-    neighbours{3} = [x-1, y];
-    neighbours{4} = [x, y-1];
-    % delete all invalid moves
-    for i = 1:numel(neighbours)
-        if neighbours{i}(1) > width(board) || ...
-           neighbours{i}(2) > height(board) || ...
-           neighbours{i}(1) < 0 || neighbours{i}(2) < 0 || ...
-           board(neighbours{i}(1), neighbours{i}(2)) == 1
-            neighbours{i} = [];
-        end
+    board_size = size(board, 2);
+    neighbours = {};
+    if (x+1) > 1 && (x+1) <= board_size && board((x+1), y) == 0
+        neighbours = [neighbours, [(x+1), y]];
+    end
+    if (x-1) > 1 && (x-1) <= board_size && board((x-1), y) == 0
+        neighbours = [neighbours, [(x-1), y]];
+    end
+    if (y+1) > 1 && (y+1) <= board_size && board(x, (y+1)) == 0
+        neighbours = [neighbours, [x, (y+1)]];
+    end
+    if (y-1) > 1 && (y-1) <= board_size && board(x, (y-1)) == 0
+        neighbours = [neighbours, [x, (y-1)]];
     end
 end
 
