dbb26b409382bb26d49797c86f01b9f345a7f996
Tom Stirnkorb <tom@stirnkorb.me>
2024-05-05 17:07:42 +0000

    [d4e3] add some comments

--- day4/exercise_3/linearSearch.m
+++ day4/exercise_3/linearSearch.m
@@ -1,6 +1,7 @@
 function xmin = linearSearch(f, x, delta)
 dim = length(x);
 xmin = zeros(dim, 1);
+% Loop over all dimensions
 for i = 1:dim
     x1 = x;
     x2 = x;
@@ -8,6 +9,7 @@ for i = 1:dim
     x2(i) = x(i) + delta;
     f1 = f(x1);
     f2 = f(x2);
+    % For the current dimension, choose the point that minimizes the function
     if f1 < f2
         xmin(i) = x1(i);
     else
