0b9179977f804a9df6cbacb28eaff8a841f4737a
Tom Stirnkorb <tom@stirnkorb.me>
2024-05-07 13:04:11 +0000

    [d6e2] fix constraint function, delete unnecessary stuff

--- day6/exercise_2/exercise_2.m
+++ day6/exercise_2/exercise_2.m
@@ -1,17 +1,11 @@
 clear; clc;
 
-n = 3; % change this to the wanted test case
+n = 2; % change this to the wanted test case
 
 x0 = zeros(1,n); % starting point
 
-lb = -1*ones(1,n); % lower bound. Can at most be -1
-ub = ones(1,n); % upper bound
-
-tolfun = 10^(-n*5); % set tolerances with respect to n as we need finer tolerances for higher dimensions
-tolx = 10^(-n*5);
-
-options = optimset('Display','iter', 'TolFun', tolfun, 'TolX', tolx); % Set tolerances and display output
-[x,f] = fmincon(@fn,x0,[],[],[],[],lb,ub,@normcon, options);
+options = optimset('Display','iter'); % Set tolerances and display output
+[x,f] = fmincon(@fn,x0,[],[],[],[],[],[],@normcon, options);
 disp(['final result: [',num2str(x),']',]); disp(['final function value: ',num2str(f)]);
 
 % show a plot in the 2D case
@@ -25,7 +19,7 @@ end
 
 % constraint function for ||x|| <= 1
 function [c, ceq] = normcon(x)
-c = norm(x) - 1;
+c = norm(x)^2 - 1;
 ceq = [];
 end
 
