926d1c4f8445f25bf12b4595047e925e4e9705c3
Tom Stirnkorb <tom@stirnkorb.me>
2024-05-14 21:46:36 +0000

    [d9e2] not quite working

--- day9/exercise_1/exercise_1.m
+++ day9/exercise_1/exercise_1.m
@@ -16,272 +16,208 @@ switch fn_select
         plot_limits = [0, 1; 0, 1];
 end
 
-p_c = 0.9;
-p_m = 0.1;
-mutation_beta = 3;
-sigma_share = 0.5;
-alpha = 1;
-sigma_mate = 3 * sigma_share;
-N = 100;
-n_elite = round(N * 0.4);
-p_hist = zeros(1, N);
-p_break = 0.01;
+% Parameters
+N = 100; % Population size
+k_max = 300; % Maximum number of iterations
+p_c = 0.8; % Crossover probability
+p_m = 0.1; % Mutation probability
+n_elite = 40; % Number of elite individuals
+sigma_s = 0.04; % Sharing function threshold
+alpha_s = 1; % Sharing function shape parameter
+sigma_m = 0.1; % Crossover threshold
+d_p_max = 1; % Maximum length of the pareto front
+pause_time = 0.2;
 
-num_iterations = 200;
+rng default
 
-P = create_population(N, lim);
-P = [P, evaluate_function(fn, P)];
-[shared_fitnesses, q] = calculate_shared_fitnesses(P, alpha, sigma_share);
-P = [P, shared_fitnesses, q];
-P = sortrows(P, 3, 'descend');
-nondom_idx = find(cell2mat(P(:,4)) == 0);
-nondoms = P(nondom_idx, :);
+% Initialization
+X = lim(:,1) + (lim(:,2) - lim(:,1)) .* rand(dim, N);
+F = fn(X);
+D = zeros(1, N);
+for i = 1:N
+    D(i) = count_dominated_by(F(:,i), F);
+end
+SF = compute_shared_fitnesses(X, D, sigma_s, alpha_s);
+X_elite = X(:,1:n_elite); % Might want to sort before selecting elite
+% Initial Plot
+figure;
+pl = scatter(F(1,:), F(2,:), 'filled');
+% xlim(plot_limits(1,:));
+% ylim(plot_limits(2,:));
 
-elite = P(1:n_elite, :);
-
-figure
-F = cell2mat(P(:,2));
-E = cell2mat(elite(:,2));
-hold on
-pl1 = plot(F(:,1), F(:,2), 'ob');
-pl2 = plot(E(:,1), E(:,2), 'or');
-pl1.XDataSource = 'F(:,1)';
-pl1.YDataSource = 'F(:,2)';
-pl2.XDataSource = 'E(:,1)';
-pl2.YDataSource = 'E(:,2)';
-xlim(plot_limits(1,:));
-ylim(plot_limits(2,:));
-
-k = 0;
-while k < num_iterations
-    k = k + 1;
-    
+% Main loop
+for k = 1:k_max
     % calculate sigma_share
-    F_z = [min(F(:,1)), min(F(:,2))];
+    F_z = [min(F(1,:)), min(F(2,:))];
     % find the two points with the biggest distance in F
     combs = nchoosek(1:N, 2);
     max_dist = 0;
     best_comb = [0, 0];
     for i = 1:size(combs, 1)
-        d = norm(F(combs(i,1),:) - F(combs(i,2),:));
+        d = norm(F(:, combs(i,1)) - F(:, combs(i,2)));
         if d > max_dist
             max_dist = d;
             best_comb = combs(i,:);
         end
     end
-    F_x = F(best_comb(1),:);
-    F_y = F(best_comb(2),:);
+    if best_comb == [0, 0]
+        error("No best combination found");
+    end
+    F_x = F(:, best_comb(1));
+    F_y = F(:, best_comb(2));
+    if F_x(1) > F_y(1)
+        temp = F_x;
+        F_x = F_y;
+        F_y = temp;
+    end
     d1 = norm(F_x - F_z);
     d2 = norm(F_y - F_z);
     d_min = sqrt(d1^2 + d2^2);
     d_max = d1 + d2;
     d_k = (d_min + d_max) / 2;
+    if d_k > d_p_max
+        d_p_max = d_k;
+    end
     sigma_share = N^(1/1-m) * d_k;
-    sigma_mate = 3 * sigma_share;
+    sigma_mate = 3 * N^(1/1-m) * d_k;
 
-    pll1 = plot([F_x(1), F_z(1)], [F_x(2), F_z(2)], 'g');
-    pll2 = plot([F_y(1), F_z(1)], [F_y(2), F_z(2)], 'g');
-    pll3 = plot([F_x(1), F_y(1)], [F_x(2), F_y(2)], 'g');
-    
-    parents = parent_selection(P);
-    offspring = crossover(parents, p_c, sigma_mate);
-    offspring = mutation(offspring, p_m, mutation_beta, lim);
-    offspring = mat2cell(offspring, ones(1,N), height(lim));
-    offspring = [offspring, evaluate_function(fn, offspring), cell(N,1), cell(N,1)];
-    P = [elite; offspring];
-    [shared_fitnesses, q] = calculate_shared_fitnesses(P, alpha, sigma_share);
-    P(:,3) = shared_fitnesses;
-    P(:,4) = q;
-    P = sortrows(P, 3, 'descend');
-    P = P(1:N, :);
-    elite = P(1:n_elite, :);
-    F = cell2mat(P(:,2));
-    E = cell2mat(elite(:,2));
+    % Selection
+    p_idx = tournament_selection(D, SF);
+    X_new = crossover(X(:,p_idx), F(:, p_idx), p_c, sigma_m);
+    X_new = mutation(X_new, p_m, lim);
+    X = [X_new, X_elite];
+    F = fn(X);
+    D = zeros(1, size(X, 2));
+    for i = 1:size(X, 2)
+        D(i) = count_dominated_by(F(:,i), F);
+    end
+    SF = compute_shared_fitnesses(X, D, sigma_s, alpha_s);
+    [~, idx] = sort(SF, "descend");
+    X = X(:,idx(1:N));
+    F = F(:,idx(1:N));
+    D = D(idx(1:N));
+    SF = SF(idx(1:N));
+    X_elite = X(:,1:n_elite);
+    % Plot
+    hold on;
+    delete(pl);
+    if exist('pl2', 'var')
+        delete(pl2);
+    end
+    pl = plot(F(1,:), F(2,:), 'bo');
+    pl2 = plot(F(1,1:n_elite), F(2,1:n_elite), 'ro');
+    xlim(plot_limits(1,:));
+    ylim(plot_limits(2,:));
     title(['Iteration ', num2str(k)]);
-    refreshdata
-    drawnow limitrate
-    fprintf('Iteration %d, sigma_s: %.4f, d_k = %0.3f\n', k, sigma_share, d_k);
-    delete(pll1);
-    delete(pll2);
-    delete(pll3);
+    drawnow limitrate;
 
-    if k < 10
-        continue;
-    end
-    nondom_idx_new = find(cell2mat(P(:,4)) == 0);
-    nondoms_new = P(nondom_idx_new, :);
-    n_dash = 0;
-    for i = 1:height(nondoms_new)
-        for j = 1:height(nondoms)
-            if is_dominated_by(nondoms{j,2}, nondoms_new{i,2})
-                n_dash = n_dash + 1;
-                break;
-            end
-        end
-    end
-    p = n_dash / length(nondom_idx_new);
-    p_hist(k) = p;
-    p_avg = mean(p_hist(k-9:k));
-    fprintf('p: %.4f, p_avg: %4f\n', p, p_avg);
-    if p_avg < p_break
-        break;
-    end
-    nondoms = nondoms_new;
+    fprintf("Iteration %d \t d_k: %f \t sigma_share: %f \t sigma_mate: %f\n", k, d_k, sigma_share, sigma_mate)
 
-    fprintf("\n");
+    pause(pause_time);
 end
 
-function f = evaluate_function(fn, x)
-if iscell(x)
-    f = fn(cell2mat(x)');
-else
-    f = fn(x');
-end
-f = num2cell(f', 2);
-end
-
-function d = is_dominated(f1, F)
+function d = count_dominated_by(f, set)
+% Count the number of solutions in set that are dominating f
 d = 0;
-for i = 1:height(F)
-    if is_dominated_by(f1, F(i, :))
+for i = 1:size(set, 2)
+    if all(f >= set(:,i)) && any(f > set(:,i))
         d = d + 1;
     end
 end
 end
 
-function d = is_dominated_by(f1, f2)
-% true if f2 dominates f1
-d = all(f2 <= f1) && any(f2 < f1);
-end
-
-function P = create_population(N, lim)
-dim = height(lim);
-P = zeros(N, dim);
-for i = 1:dim
-    P(:,i) = lim(i,1) + (lim(i,2) - lim(i,1)) * rand(1, N);
-end
-P = mat2cell(P, ones(1,N), dim);
-end
-
-function SF = sharing_function(f1, f2, alpha, sigma_share)
-d = norm(f1 - f2);
-if d < sigma_share
-    SF = 1 - (d / sigma_share)^alpha;
-else
-    SF = 0;
-end
-end
-
-function SF = shared_fitness_sum(f, F, alpha, sigma_share)
-N = height(F);
-SF = 0;
+function SF = compute_shared_fitnesses(X, D, sigma, alpha)
+% Compute the shared fitness of a solution
+% X: population
+% F: objective values of the population
+% D: domination count of the population
+% sigma: sharing threshold
+% alpha: sharing function shape parameter
+N = size(X, 2);
+SF = zeros(1, N);
 for i = 1:N
-    SF = SF + sharing_function(f, F(i,:), alpha, sigma_share);
+    SF(i) = 0;
+    for j = 1:N
+        d = norm(X(:,i) - X(:,j));
+        if d < sigma
+            SF(i) = SF(i) + (1 - d/sigma)^alpha;
+        end
+    end
 end
-end
-
-function [f, q] = calculate_shared_fitnesses(P, alpha, sigma_share)
-% P has to be a cell array with the individuals in the first row and function values in the second row
-N = height(P);
-F = cell2mat(P(:,2));
-f = zeros(N, 1);
-q = zeros(N, 1);
-for i = 1:N
-    q(i) = is_dominated(F(i,:), F);
-    r = 1 + q(i);
-    f(i) = 1 / r;
-    sum_sf = shared_fitness_sum(F(i,:), F, alpha, sigma_share);
-    f(i) = f(i) / sum_sf;
-end
-f = num2cell(f);
-q = num2cell(q);
+SF = (1 ./ (1 + D)) ./ SF;
 end
 
-function S = parent_selection(P)
-P_orig = P;
-S = [P_orig; P_orig];
-SF = cell2mat(P(:,3));
-P = cell2mat(P(:,1));
-D = cell2mat(P_orig(:,4));
-N = height(P);
+function idx = tournament_selection(D, SF)
+% Perform tournament selection
+% D: dominated by count of the candidates
+% SF: shared fitness of the candidates
+N = size(D, 2);
+idx = zeros(1, N);
 for i = 1:2*N
-    c = randi(N, 1, 2);
-    c1_dominated = D(c(1)) > 0;
-    c2_dominated = D(c(2)) > 0;
+    r = randi(N, 1, 2);
+    c1_dominated = D(r(1)) > 0;
+    c2_dominated = D(r(2)) > 0;
     if c1_dominated == c2_dominated
-        f1 = SF(c(1));
-        f2 = SF(c(2));
+        f1 = SF(r(1));
+        f2 = SF(r(2));
         if f1 > f2
-            S(i, :) = P_orig(c(1), :);
+            idx(i) = r(1);
         else
-            S(i, :) = P_orig(c(2), :);
+            idx(i) = r(2);
         end
+    elseif ~c1_dominated
+        idx(i) = r(1);
     else
-        if c1_dominated == false
-            S(i, :) = P_orig(c(1), :);
-        else
-            S(i, :) = P_orig(c(2), :);
-        end
+        idx(i) = r(2);
     end
 end
 end
 
-function C = crossover(P, p_c, sigma_mate)
-F = cell2mat(P(:,3));
-P = cell2mat(P(:,1));
-N = height(P);
-dim = width(P);
-C = zeros(N/2, dim);
+function X_new = crossover(X, F, p_c, sigma_m)
+% Perform crossover
+% X: parents
+% F: objective values of the parents
+% p_c: crossover probability
+% sigma_m: mating threshold
+dim = size(X, 1);
+N = size(X, 2);
+X_new = zeros(dim, N/2);
 for i = 1:N/2
-    p1 = P(2*i-1,:);
-    f1 = F(2*i-1,:);
-    if rand() < p_c
-        found = false;
-        % search for mate within sigma_mate
-        randp = randperm(N);
-        for j = 1:N
-            k = randp(j);
-            if i ~= j
-                p2 = P(k,:);
-                f2 = F(k,:);
-                d = norm(f1 - f2);
-                if d < sigma_mate && d ~= 0
-                    found = true;
+    if rand < p_c
+        p1 = i;
+        p2 = 2 * i;
+        d = norm(F(:, i) - F(:, 2*i));
+        if d ~= 0 && d > sigma_m
+            searchperm = randperm(N/2) + N/2;
+            for j = searchperm
+                d = norm(F(:, i) - F(:, j));
+                if d <= sigma_m && d ~= 0
+                    p2 = j;
                     break;
                 end
             end
         end
-        % if no mate was found, choose random mate
-        if ~found
-            p2 = P(ceil(N*rand()),:);
-        end
-        % intermediate crossover
-        for k = 1:dim
-            alpha = rand();
-            C(i,k) = alpha*p1(k) + (1-alpha)*p2(k);
-        end
+        alpha = rand;
+        X_new(:, i) = alpha * X(:, p1) + (1 - alpha) * X(:, p2);
     else
-        % copy one parent
-        C(i,:) = p1;
+        % copy parent
+        X_new(:, i) = X(:, i);
     end
 end
 end
 
-function R = mutation(P, p_m, beta, lim)
-N = height(P);
-dim = width(P);
-for j = 1:N
-    if rand() < p_m
-        for k = 1:dim
-            r = rand();
-            if r > 0.5
-                delta_x = (lim(k,2) - P(j,k))*(2*(r - 0.5))^beta;
-            else
-                delta_x = (lim(k,1) - P(j,k))*(2*(0.5-r))^beta;
-            end
-            P(j,k) = P(j,k) + delta_x;
-        end
+function X_new = mutation(X, p_m, lim)
+% Perform mutation
+% X: population to mutate
+% p_m: mutation probability
+% lim: limits of the parameters
+dim = size(X, 1);
+N = size(X, 2);
+X_new = X;
+for i = 1:N
+    if rand < p_m
+        % perform mutation
+        X_new(:, i) = lim(:,1) + (lim(:,2) - lim(:,1)) .* rand(dim, 1);
     end
 end
-R = P;
 end
