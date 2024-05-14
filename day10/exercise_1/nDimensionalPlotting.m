526bb0536348545bdecc8f75c1b39d9c78ab912a
CoolEou <115168150+CoolEou@users.noreply.github.com>
2024-05-14 11:02:39 +0000

    d10e1 Finished

---
+++ day10/exercise_1/nDimensionalPlotting.m
@@ -0,0 +1,19 @@
+function nDimensionalPlotting(sampleSet)
+    n = size(sampleSet, 2); % Dimension of sampleSet
+    sampleCount = size(sampleSet, 1); % Amount of samples
+    subSpaceCount = (n*(n-1)/2); % Amount of subspaces
+    subSpaces = nchoosek(1:n,2); % A matrix containing all the subspaces
+    
+    plotMatrixSize = factor(subSpaceCount);
+    if size(plotMatrixSize, 2) > 2
+        plotMatrixSize = [prod(plotMatrixSize(1:end-1)), plotMatrixSize(end)];
+    end
+
+    figure
+    for i = 1:subSpaceCount
+        subplot(plotMatrixSize(1), plotMatrixSize(2), i)
+        scatter(sampleSet(:,subSpaces(i,1)), sampleSet(:,subSpaces(i,2)))
+        xlabel(['x_{', num2str(subSpaces(i,1)), '}'], 'FontWeight', 'bold')
+        ylabel(['x_{', num2str(subSpaces(i,2)), '}'], 'FontWeight', 'bold')
+    end
+end
